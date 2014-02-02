a696734d93ccf90c9e4d7e7513d60f4c001e9d53
Andrei Avram <andrei.avram@gmail.com>
2014-02-02 07:48:15 +0200

    Added file skeleton, added setup.py, updated .gitignore

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='django_goodies',
+    version='0.0.1',
+    packages=['goodies', ],
+    url='https://github.com/andreiavram/django_goodies',
+    license='MIT',
+    author='Andrei Avram',
+    author_email='andrei.avram@gmail.com',
+    description='A collection of useful django views, forms, widgets that work with and around bootstrap to make building UIs easier',
+    install_requires=['Django>=1.4']
+)
